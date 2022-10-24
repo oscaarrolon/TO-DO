44f84906f3c1fcc7bb8ace842f7f8fb8c83565c0
Oscar Rolón <oscarpcpi2gmai@gmail.com>
2022-10-24 20:00:22 +0200

    verion 1

--- js/funciones.js
+++ js/funciones.js
@@ -1,11 +1,62 @@
-const form1 = document.querySelector('.form')
-const introducirTarea = document.querySelector('.prioridad1')
-const selectPrioridad = document.querySelector('#prioridad')
-const guardar = document.querySelector('.btnGuardar')
+
+const container = document.getElementById('container')
+const btnGuardar = document.getElementById('btnGuardar')
+const inputTarea = document.getElementById('inputTarea')
+const selectPrioridad = document.getElementById('selectPrioridad')
 let tareas = []
 
-guardar.addEventListener('click', () => {
-    tareas.push(introducirTarea.value)
-    const strTareas = JSON.stringify(tareas);
-    localStorage.setItem('arrTareas', strTareas)
-});
+pintarObj = (obj) => {
+
+    const cajaTareas = document.createElement('div')
+    cajaTareas.className += 'd-flex'
+    cajaTareas.className += ' justify-content-between'
+    cajaTareas.className += ' border border-warning'
+    cajaTareas.className += ' bg-info'
+
+    const h3Tareas = document.createElement('h3')
+    h3Tareas.className += "m-3"
+    h3Tareas.innerText = obj.tarea
+
+    const btnEliminar = document.createElement('button')
+    btnEliminar.className = "btn btn-danger"
+    btnEliminar.innerText = 'Eliminar'
+
+    btnEliminar.addEventListener('click', (event) => {
+
+        if (event.target) {
+            eliminarObjeto()
+        }
+
+
+
+    })
+
+    cajaTareas.append(h3Tareas, btnEliminar)
+
+    return container.append(cajaTareas)
+
+};
+
+btnGuardar.addEventListener('click', () => {
+
+    const objetoTarea = {
+        tarea: inputTarea.value,
+        prioridad: selectPrioridad.value
+    }
+    tareas.push(objetoTarea)
+    pintarObj(objetoTarea)
+})
+
+const pintarLista = (arr) => {
+    arr.forEach(obj => pintarObj(obj))
+
+}
+const eliminarObjeto = (arr) => {
+
+    arr.splice(i, 1)
+
+    return arr
+
+}
+
+
