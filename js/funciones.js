69a28af80e80879b563d1894df64854dafe4a468
Oscar Rolón <oscarpcpi2gmai@gmail.com>
2022-10-24 22:31:43 +0200

    no me ha dado tiempo de acabar

--- js/funciones.js
+++ js/funciones.js
@@ -3,60 +3,80 @@ const container = document.getElementById('container')
 const btnGuardar = document.getElementById('btnGuardar')
 const inputTarea = document.getElementById('inputTarea')
 const selectPrioridad = document.getElementById('selectPrioridad')
+const filtrarTarea = document.getElementById('filtrarTarea')
+
+
 let tareas = []
+let i = 0
 
-pintarObj = (obj) => {
 
-    const cajaTareas = document.createElement('div')
-    cajaTareas.className += 'd-flex'
-    cajaTareas.className += ' justify-content-between'
-    cajaTareas.className += ' border border-warning'
-    cajaTareas.className += ' bg-info'
-
-    const h3Tareas = document.createElement('h3')
-    h3Tareas.className += "m-3"
-    h3Tareas.innerText = obj.tarea
-
-    const btnEliminar = document.createElement('button')
-    btnEliminar.className = "btn btn-danger"
-    btnEliminar.innerText = 'Eliminar'
-
-    btnEliminar.addEventListener('click', (event) => {
-
-        if (event.target) {
-            eliminarObjeto()
-        }
+let tarea = ''
+let prioridad = ''
 
 
 
-    })
 
-    cajaTareas.append(h3Tareas, btnEliminar)
 
-    return container.append(cajaTareas)
+pintarObjetos = (arr) => {
+
+    arr.forEach((obj => {
+
+
+        obj.id = i
+        const cajaTareas = document.createElement('div')
+        cajaTareas.className += 'd-flex'
+        cajaTareas.className += ' justify-content-between'
+        cajaTareas.className += ' border border-warning'
+        cajaTareas.className += ' bg-info'
+
+        const h3Tareas = document.createElement('h3')
+        h3Tareas.className += "m-3"
+        h3Tareas.innerText = obj.tarea
+
+        const btnEliminar = document.createElement('button')
+        btnEliminar.className = "btn btn-danger"
+        btnEliminar.innerText = 'Eliminar'
+
+        btnEliminar.addEventListener('click', (event) => {
+
+            if (event.target) {
+                cajaTareas.innerHTML = '';
+
+
+            }
+        })
+
+        cajaTareas.append(h3Tareas, btnEliminar)
+        i++
+        return container.append(cajaTareas)
+
+    }))
 
 };
 
-btnGuardar.addEventListener('click', () => {
+btnGuardar.addEventListener('click', (event) => {
+
+    tarea = inputTarea.value
+    prioridad = selectPrioridad.value
+
 
     const objetoTarea = {
-        tarea: inputTarea.value,
-        prioridad: selectPrioridad.value
+        tarea: tarea,
+        prioridad: prioridad
     }
+
+
+
     tareas.push(objetoTarea)
-    pintarObj(objetoTarea)
+    let nuevaLista = tareas.slice(-1)
+    pintarObjetos(nuevaLista)
 })
 
-const pintarLista = (arr) => {
-    arr.forEach(obj => pintarObj(obj))
 
-}
-const eliminarObjeto = (arr) => {
+filtrarTarea.addEventListener('input', (event) => {
 
-    arr.splice(i, 1)
+    const filtro = event.target.value
+    pintarObjetos(tarea)
 
-    return arr
-
-}
-
+})
 
